de7d867ea7125f1bd788661a7cf02f089911ede4
oueue <39622401+oueue@users.noreply.github.com>
2019-11-18 20:42:14 +0800

    Create calculator.java

---
+++ calculator/calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+      new MainWindow();
+      Scanner in = new Scanner(System.in);
+    }
+}
